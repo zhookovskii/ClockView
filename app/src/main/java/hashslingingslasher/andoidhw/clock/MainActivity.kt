dccec5c39930b5df4f156d98a743652f328bb3d8
zhookovskii <98118965+zhookovskii@users.noreply.github.com>
2023-03-05 18:14:21 +0300

    Update MainActivity.kt

--- app/src/main/java/hashslingingslasher/andoidhw/clock/MainActivity.kt
+++ app/src/main/java/hashslingingslasher/andoidhw/clock/MainActivity.kt
@@ -2,12 +2,6 @@ package hashslingingslasher.andoidhw.clock
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.TextView
-import androidx.lifecycle.coroutineScope
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.delay
-import kotlinx.coroutines.launch
-import kotlinx.coroutines.withContext
 
 class MainActivity : AppCompatActivity() {
 
